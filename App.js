a25580558f4ef29a7e9a08f4edbf6c60f4dce566
unknown <albertlemebrito@protonmail.com>
2024-03-18 23:38:30 -0300

    Criação projeto

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Iniciando o Mines</Text>
+      <Text>Tamanho da grade: {params.getRowsAmount()}X{params.getColumnsAmount()}</Text>
+      <Field></Field>
+      <Field opened></Field>
+      <Field opened nearMines={1}></Field>
+      <Field opened nearMines={2}></Field>
+      <Field opened nearMines={3}></Field>
+      <Field opened nearMines={6}></Field>
       <StatusBar style="auto" />
     </View>
   );
